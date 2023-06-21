73f4695b37827d16b7d1a50d94c2fca2c9cdc52e
yashpurkar <109805926+yash-purkar@users.noreply.github.com>
2023-06-21 16:11:27 +0530

    update: update title

--- src/Components/SinglePost/SingleComment/SingleComment.js
+++ src/Components/SinglePost/SingleComment/SingleComment.js
@@ -4,26 +4,37 @@ import { UseData } from '../../../Contexts/DataContext';
 import { AiFillDelete } from 'react-icons/ai';
 import { UsePost } from '../../../Contexts/ModalContext';
 import { deleteCommentHandler } from '../../../Services/PostServices';
+import { useNavigate } from 'react-router';
 
 export const SingleComment = ({ comment, postId }) => {
   const { _id, username, text } = comment;
+  const navigate = useNavigate()
 
-  const { dataState: { users, posts }, dataDispatch, isDarkMode } = UseData();
+  const { dataState: { users, posts }, dataDispatch, isDarkMode, setIsLoading } = UseData();
 
   const socialUser = JSON.parse(localStorage.getItem("socialUser"));
   const socialToken = localStorage.getItem("socialToken")
+
+  const handleUserProfile = (userHandler) => {
+    navigate(`/user-profile/${userHandler}`)
+    window.scrollTo(0, 0)
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 300)
+  }
 
   const handleDeleteComment = () => {
     deleteCommentHandler(postId, _id, socialToken, dataDispatch)
   }
 
-  const { firstName, lastName, profile_photo } = users?.find(el => el?.username === username) || {};
+  const { firstName, lastName, profile_photo, userHandler } = users?.find(el => el?.username === username) || {};
   return (
     <div className='comment-card flex'>
-      <span ><img src={profile_photo} alt='user-profile' className='user-comment-profile' /></span>
+      <span onClick={() => handleUserProfile(userHandler)} className='cursor-pointer'><img src={profile_photo} alt='user-profile' className='user-comment-profile' /></span>
       <div className={`user-comment letter-spacing-1 flex justify-between ${isDarkMode && "bg-dark"}`}>
         <div className='comment-info'>
-          <p className={`comment-user-name ${isDarkMode && "color-white"}`}>{firstName} {lastName}</p>
+          <p onClick={() => handleUserProfile(userHandler)} className={`cursor-pointer comment-user-name ${isDarkMode && "color-white"}`}>{firstName} {lastName}</p>
 
           <p className={`comment-desc ${isDarkMode && "color-white"}`}>{text}</p>
         </div>
